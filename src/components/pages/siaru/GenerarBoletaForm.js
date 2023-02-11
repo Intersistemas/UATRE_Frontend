e77c0336efb50db66e2e60f6b007c237effc9a22
Rodrigo Lopez <rodrigo@binarianet.com>
2023-02-11 16:04:30 -0300

    Parametro intereses diarios y calculo de los mismos

--- src/components/pages/siaru/GenerarBoletaForm.js
+++ src/components/pages/siaru/GenerarBoletaForm.js
@@ -9,6 +9,7 @@ import Celda from "../../ui/Grilla/Celda";
 import Select from "../../ui/Select/Select";
 import DateTimePicker from "../../ui/DateTimePicker/DateTimePicker";
 import TextField from "@mui/material/TextField";
+import dayjs from "dayjs";
 
 const GenerarBoletaForm = (props) => {
 	const config = { ...props.config };
@@ -18,13 +19,17 @@ const GenerarBoletaForm = (props) => {
 	const onConfirma = config.onConfirma ?? ((data) => {});
 	const [tiposPagos, setTiposPagos] = useState([]);
 	const { isLoading, error, sendRequest: request } = useHttp();
+	const [params, setParams] = useState({
+		interesesDiariosPosteriorFechaPago: 0,
+	});
+	const joinParams = (nParams) => setParams({ ...params, ...nParams });
 	const [data, setData] = useState({
 		empresasEstablecimientosId: establecimiento.id,
 		periodo: 0,
-		fecha: "",
+		fecha: dayjs().format("YYYY-MM-DD"),
 		cantidadTrabajadores: 0,
 		totalRemuneraciones: 0,
-		fechaPagoEstimada: "",
+		fechaPagoEstimada: null,
 		interesImporte: 0,
 		interesPorcentaje: 0,
 		interesNeto: 0,
@@ -32,18 +37,50 @@ const GenerarBoletaForm = (props) => {
 		conveniosId: 0,
 		tipoLiquidacion: null,
 	});
+	const joinData = (nData) => {
+		let newData = { ...data, ...nData };
+		// calculos
+		newData.interesPorcentaje =
+			tiposPagos.find((r) => r.id === newData.tipoLiquidacion)?.porcentaje ?? 0;
+		newData.interesImporte =
+			newData.totalRemuneraciones * newData.interesPorcentaje;
+		if (newData.periodo > 0 && newData.fechaPagoEstimada != null) {
+			let fpeDjs = dayjs(newData.fechaPagoEstimada);
+			let dias = fpeDjs.diff(
+				Formato.Mascara(newData.periodo, "####-##-15"),
+				"day"
+			);
+			if (dias > 0) {
+				newData.interesNeto =
+					newData.totalRemuneraciones *
+					params.interesesDiariosPosteriorFechaPago *
+					dias;
+				console.log("newData.interesNeto", newData.interesNeto);
+				newData.interesNeto += newData.interesImporte;
+			}
+		}
+		setData(newData);
+	};
 
 	useEffect(() => {
+		request(
+			{
+				baseURL: "SIARU",
+				endpoint: `/Parametros?Nombre=InteresesDiariosPosteriorFechaPago`,
+				method: "GET",
+			},
+			async (response) =>
+				joinParams({
+					interesesDiariosPosteriorFechaPago: Formato.Decimal(response?.valor ?? 0),
+				})
+		);
 		request(
 			{
 				baseURL: "SIARU",
 				endpoint: `/Siaru_TiposPagos`,
 				method: "GET",
 			},
-			async (response) => {
-				console.log("response", response);
-				setTiposPagos(response);
-			}
+			async (response) => setTiposPagos(response)
 		);
 	}, [request]);
 
@@ -53,20 +90,7 @@ const GenerarBoletaForm = (props) => {
 	};
 
 	const handleTipoLiquidacionChange = (tipoPagoId) => {
-		setData({ ...data, tipoLiquidacion: tipoPagoId });
-	};
-
-	const handlePeriodoChange = (fecha) => {
-		let newData = { ...data };
-		console.log("fecha", fecha);
-		if (fecha == null || isNaN(fecha.$y)) newData.periodo = 0;
-		else
-			newData.periodo = parseInt(
-				Formato.Mascara(fecha.$y, "####") + Formato.Mascara(fecha.$M + 1, "##"),
-				10
-			);
-		console.log("newData.periodo", newData.periodo);
-		setData(newData);
+		joinData({ tipoLiquidacion: tipoPagoId });
 	};
 
 	let periodo = "";
@@ -75,12 +99,16 @@ const GenerarBoletaForm = (props) => {
 	return (
 		<Modal onClose={onCancela}>
 			<Renglon centro>
-				<div className={styles.titulo}>
-					Generando boleta
-					<br />
-					Eempresa {empresa.razonSocial}
-					<br />
-					Establecimiento {establecimiento.nombre}
+				<div className={styles.titulo}>Generando boleta</div>
+			</Renglon>
+			<Renglon>
+				<div className={styles.subtitulo}>
+					<span>Eempresa</span> {empresa.razonSocial}
+				</div>
+			</Renglon>
+			<Renglon>
+				<div className={styles.subtitulo}>
+					<span>Establecimiento</span> {establecimiento.nombre}
 				</div>
 			</Renglon>
 			<Renglon>
@@ -101,7 +129,9 @@ const GenerarBoletaForm = (props) => {
 						type="month"
 						label="Periodo"
 						value={periodo}
-						onChange={handlePeriodoChange}
+						onChange={(f) =>
+							joinData({ periodo: Formato.Entero(f?.format("YYYYMM") ?? 0) })
+						}
 					/>
 				</Celda>
 			</Renglon>
@@ -117,8 +147,7 @@ const GenerarBoletaForm = (props) => {
 						}}
 						value={data.cantidadTrabajadores}
 						onChange={(e) =>
-							setData({
-								...data,
+							joinData({
 								cantidadTrabajadores: Formato.Entero(e.target.value),
 							})
 						}
@@ -133,10 +162,21 @@ const GenerarBoletaForm = (props) => {
 						InputLabelProps={{ shrink: true }}
 						value={data.totalRemuneraciones}
 						onChange={(e) =>
-							setData({
-								...data,
+							joinData({
 								totalRemuneraciones: Formato.Decimal(e.target.value),
 							})
+						}
+					/>
+				</Celda>
+			</Renglon>
+			<Renglon>
+				<Celda width={50}>
+					<DateTimePicker
+						type="date"
+						label="Fecha pago estimada"
+						value={data.fechaPagoEstimada}
+						onChange={(f) =>
+							joinData({ fechaPagoEstimada: f?.format("YYYY-MM-DD") ?? null })
 						}
 					/>
 				</Celda>
