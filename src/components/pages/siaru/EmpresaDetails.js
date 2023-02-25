5856ccf4c68e4bb1c6e831b80d7d95c8e38e0c60
Rodrigo Lopez <rodrigo@binarianet.com>
2023-02-24 21:43:58 -0300

    Pantalla Empresas reformada al nuevo diseño

--- src/components/pages/siaru/EmpresaDetails.js
+++ src/components/pages/siaru/EmpresaDetails.js
@@ -1,45 +1,89 @@
-import React from 'react';
+import React from "react";
+import Formato from "../../helpers/Formato";
+import Grid from "../../ui/Grid/Grid";
 import styles from "./EmpresaDetails.module.css";
 
 const EmpresaDetails = (props) => {
 	const config = props.config;
-	const data = config.data;
-
-	if (data.domicilioCalle == null) data.domicilioCalle = "";
-	if (data.domicilioNumero == null) data.domicilioNumero = 0;
-	if (data.domicilioPiso == null) data.domicilioPiso = "";
-	if (data.domicilioDpto == null) data.domicilioDpto = "";
-	if (data.domicilioSector == null) data.domicilioSector = "";
-	if (data.domicilioTorre == null) data.domicilioTorre = "";
-	if (data.domicilioManzana == null) data.domicilioManzana = "";
-
-	let domicilio = "";
-
-	if (data.domicilioCalle !== 0) {
-		if (domicilio) domicilio = `${domicilio} `;
-		domicilio = `${domicilio} Calle ${data.domicilioCalle}`;
-	}
-	if (data.domicilioNumero !== 0) {
-		if (domicilio) domicilio = `${domicilio} `;
-		domicilio = `${domicilio} Nro ${data.domicilioNumero}`;
-	}
-	if (data.domicilioPiso !=="") {
-		if (domicilio) domicilio = `${domicilio} `;
-		domicilio = `${domicilio} Piso ${data.domicilioPiso}`;
-	}
-	if (data.domicilioDpto !== "") {
-		if (domicilio) domicilio = `${domicilio} `;
-		domicilio = `${domicilio} Dpto ${data.domicilioDpto}`;
-	}
+	const data = config.data ?? {};
 
 	return (
-		<div className={styles.details}>
-			<div><span>CUIT:</span> {data.cuit}</div>
-			<div><span>Razon Social:</span> {data.razonSocial}</div>
-			<div><span>Teléfono:</span> {data.telefono}</div>
-			<div><span>E-mail:</span> {data.email}</div>
-			<div><span>Domicilio:</span> {domicilio}</div>
-		</div>
+		<Grid className={`${styles.fondo} ${styles.grupo}`} col full="width">
+			<Grid full="width">
+				<Grid className={styles.titulo} grow>
+					Datos de la empresa
+				</Grid>
+			</Grid>
+			<Grid full="width">
+				<Grid block basis="051px" className={styles.label}>
+					CUIT:
+				</Grid>
+				<Grid block basis="125px" className={styles.data}>
+					{Formato.Cuit(data.cuit)}
+				</Grid>
+				<Grid block basis="110px" className={styles.label}>
+					Razon Social:
+				</Grid>
+				<Grid block basis="calc(100% - 286px)" className={styles.data}>
+					{data.razonSocial}
+				</Grid>
+			</Grid>
+			<Grid full="width">
+				<Grid className={styles.grupo} col full>
+					<Grid full="width">
+						<Grid className={styles.titulo} grow>
+							Domicilio administrativo
+						</Grid>
+					</Grid>
+					<Grid full="width">
+						<Grid block basis="055px" className={styles.label}>
+							Calle:
+						</Grid>
+						<Grid block basis="calc(100% - 286px)" className={styles.data}>
+							{data.domicilioCalle ?? ""}
+						</Grid>
+					</Grid>
+					<Grid full="width">
+						<Grid block basis="055px" className={styles.label}>
+							Nro:
+						</Grid>
+						<Grid block basis="calc(100% - 055px)" className={styles.data}>
+							{data.domicilioNumero ?? ""}
+						</Grid>
+					</Grid>
+					<Grid full="width">
+						<Grid block basis="055px" className={styles.label}>
+							Piso:
+						</Grid>
+						<Grid block basis="125px" className={styles.data}>
+							{data.domicilioPiso ?? ""}
+						</Grid>
+						<Grid block basis="055px" className={styles.label}>
+							Dpto:
+						</Grid>
+						<Grid block basis="calc(100% - 235px)" className={styles.data}>
+							{data.domicilioPiso ?? ""}
+						</Grid>
+					</Grid>
+					<Grid full="width">
+						<Grid block basis="095px" className={styles.label}>
+							Provincia:
+						</Grid>
+						<Grid block basis="calc(100% - 095px)" className={styles.data}>
+							{data.domicilioProvincia ?? ""}
+						</Grid>
+					</Grid>
+					<Grid full="width">
+						<Grid block basis="095px" className={styles.label}>
+							Localidad:
+						</Grid>
+						<Grid block basis="calc(100% - 095px)" className={styles.data}>
+							{data.domicilioProvincia ?? ""}
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
 	);
 };
 
--- src/components/pages/siaru/SiaruHandler.js
+++ src/components/pages/siaru/SiaruHandler.js
@@ -1,44 +1,95 @@
 import React, { useState, useEffect } from "react";
-import { redirect } from "react-router-dom";
 import useHttp from "../../hooks/useHttp";
+import Grid from "../../ui/Grid/Grid";
 import EmpresaDetails from "./EmpresaDetails";
-import EstablecimientosHandler from "./EstablecimientosHandler";
+import EmpresasList from "./EmpresasList";
 import styles from "./SiaruHandler.module.css";
-import {useLocation} from 'react-router-dom';
 
 const SiaruHandler = (props) => {
-	const location = useLocation();
-	const cuit = parseFloat(location.state.cuit ?? 0);
-	if (cuit == 0) redirect("/");
+	const [empresas, setEmpresas] = useState([]);
 	const [empresa, setEmpresa] = useState(null);
 	const { isLoading, error, sendRequest: request } = useHttp();
 
 	useEffect(() => {
-		const processEmpresa = async (empresaObj) => {
-			setEmpresa(empresaObj);
-		};
+		// request(
+		// 	{
+		// 		baseURL: "SIARU",
+		// 		endpoint: `/Empresas/${cuit}`,
+		// 		method: "GET",
+		// 	},
+		// 	async (response) => setEmpresas(response)
+		// );
+		setEmpresas([
+			{
+				cuit: 34618764356,
+				razonsocial: "HUAYRA SCA",
+				localidad: "Rio Negro",
+				domicilio: "AVELEYRA 338",
+			},
+			{
+				cuit: 34617797587,
+				razonsocial: "LUISITO SA",
+				localidad: "Buenos Aires",
+				domicilio: "JUJUY 766",
+			},
+			{
+				cuit: 34610675923,
+				razonsocial: "TAPE S.A.",
+				localidad: "Buenos Aires",
+				domicilio: "RODRIGUEZ PEÑA 616",
+			},
+			{
+				cuit: 34560268019,
+				razonsocial: "ASOC COOP DE LA EEA MZA-I",
+				localidad: "Corrientes",
+				domicilio: "SAN MARTIN 3853",
+			},
+		]);
+	// }, [request]);
+	}, []);
 
+	// if (isLoading) return <h1>Cargando...</h1>;
+	// if (error) return <h1>{error}</h1>;
+	// if (empresa == null) return <></>;
+
+	const fetchEmpresa = (cuit) => {
+		if ((cuit ?? 0) == 0) {
+			setEmpresa(null);
+			return;
+		}
 		request(
 			{
 				baseURL: "SIARU",
 				endpoint: `/Empresas/${cuit}`,
 				method: "GET",
 			},
-			processEmpresa
+			async (response) => setEmpresa(response)
 		);
-	}, [request]);
-
-	if (isLoading) return <h1>Cargando...</h1>;
-	if (error) return <h1>{error}</h1>;
-	if (empresa == null) return <></>;
+	};
 
 	return (
-		<>
-			<h1 className={styles.titulo}>Sistema de Aportes Rurales</h1>
-			<h2 className="subtitulo">Empresa</h2>
-			<EmpresaDetails config={{ data: empresa }} />
-			<EstablecimientosHandler config={{ empresa: empresa }} />
-		</>
+		<Grid col full>
+			<Grid full="width">
+				<h1 className={styles.titulo}>Sistema de Aportes Rurales</h1>
+			</Grid>
+			<Grid full="width" style={{ paddingBottom: "5px" }}>
+				<h2 className="subtitulo">Empresas</h2>
+			</Grid>
+			<Grid full="width" grow>
+				<Grid width="50%">
+					<EmpresasList
+						config={{
+							data: empresas,
+							onSelect: (r) => fetchEmpresa(r?.cuit),
+						}}
+					/>
+				</Grid>
+				<Grid block width="50%" style={{ paddingLeft: "5px" }}>
+					<EmpresaDetails config={{ data: empresa }} />
+				</Grid>
+			</Grid>
+			{/* <EstablecimientosHandler config={{ empresa: empresa }} /> */}
+		</Grid>
 	);
 };
 
