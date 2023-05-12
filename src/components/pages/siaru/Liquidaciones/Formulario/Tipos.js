7467902fd697e2eff6ac553f63ab54c86106b8e6
Rodrigo Lopez <rodrigo@binarianet.com>
2023-05-12 09:22:20 -0300

    "Enumerado" Tipos semanticamente correcto: https://www.sohamkamani.com/javascript/enums/

--- src/components/pages/siaru/Liquidaciones/Formulario/Tipos.js
+++ src/components/pages/siaru/Liquidaciones/Formulario/Tipos.js
@@ -1,6 +1,10 @@
-const Tipos = Object.freeze({
-	Liquidacion: "Liquidacion",
-	Tentativa: "Tentativa",
-});
+class Tipos {
+	static Liquidacion = new Tipos("Liquidacion");
+	static Tentativa = new Tipos("Tentativa");
+
+	constructor(name) {
+		this.name = name
+	}
+}
 
 export default Tipos;
