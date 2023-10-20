d17c63dee825bd402fdd57fc9188688a8e7d2fd2
Alexis Prieto <prietoalexis99@gmail.com>
2023-10-20 12:24:42 -0300

    seccion de adm de accesos

---
+++ src/components/pages/accesos/Accesos.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useState } from "react";
+import SeccionalesLista from "./lista/SeccionalesLista";
+import { Tab, Tabs } from "@mui/material";
+import styles from "./Seccionales.module.css";
+import SeccionalAutoridades from "./autoridades/SeccionalAutoridades";
+import SeccionalDocumentos from "./documentos/SeccionalDocumentos";
+
+const Accesos = (props) => {
+    const [selectedTab, setSelectedTab] = useState(0)
+
+    const handleChangeTab = (event, newValue) => {
+      setSelectedTab(newValue);
+    };
+  return ( 
+    <>
+      <div>
+        <h1 className="titulo">Administración de Accesos</h1>
+      </div>
+
+      <div>
+        <Tabs
+          value={selectedTab}
+          onChange={handleChangeTab}
+          className={styles.tabs}
+        >
+          <Tab
+            className={styles.tab}
+            style={{ backgroundColor: "#186090" }}
+            label="SECCIONALES"
+          />
+          <Tab
+            className={styles.tab}
+            style={{ backgroundColor: "#186090" }}
+            label="AUTORIDADES"
+            disabled={props.seccionalSeleccionada?.id ? false : true}
+          />
+          <Tab
+            className={styles.tab}
+            style={{ backgroundColor: "#186090" }}
+            label="DOCUMENTOS"
+            disabled={props.seccionalSeleccionada?.id ? false : true}
+          />
+        </Tabs>
+      </div>
+      {selectedTab === 0 && (
+        <div>
+          <SeccionalesLista
+            seccionales={props.seccionales}
+            onSeccionalSeleccionada={props.onSeccionalSeleccionada}
+            selectores={props.selectores}
+            selector={props.selector}
+            selectorValor={props.selectorValor}
+            onSelectorSelected={props.onSelectorSelected}
+            onSelectorValor={props.onSelectorValor}
+            onBuscarClick={props.onBuscarClick}
+            onLimpiarClick={props.onLimpiarClick}
+            onAgregarClick={props.onAgregarClick}
+          />
+        </div>
+      )}
+
+      {selectedTab === 1 && (
+        <SeccionalAutoridades
+          seccionalAutoridades={props.seccionalAutoridades}          
+          onSoloAutoridadesVigentes={props.onSoloAutoridadesVigentes}
+          onSeleccionAutoridad={props.onSeleccionAutoridad}
+        />
+      )}
+
+      {selectedTab === 2 && (
+        <SeccionalDocumentos seccionalDocumentos={props.seccionalDocumentos} />
+      )}
+    </>
+  );
+};
+
+export default Accesos;
