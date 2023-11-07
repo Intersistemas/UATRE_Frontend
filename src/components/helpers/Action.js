5bdc93d5458d24681c2fc73514bf14d912c65594
Rodrigo Lopez <rodrigo@binarianet.com>
2023-11-07 20:15:33 -0300

    Agrego posibilidad agregar otras propiedades en el constructor de Action

--- src/components/helpers/Action.js
+++ src/components/helpers/Action.js
@@ -1,6 +1,7 @@
 export default class Action {
-	constructor({ name = "", onExecute = (name) => {} } = {}) {
+	constructor({ name = "", onExecute = (name) => {}, ...x } = {}) {
 		this.name = name;
 		this.execute = () => onExecute(this.name);
+		Object.keys(x).forEach((k) => (this[k] = x[k]));
 	}
 }
