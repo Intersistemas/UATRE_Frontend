2a667e611602820b236203ae6ce35c7020941657
Rodrigo Lopez <rodrigo@binarianet.com>
2024-01-03 19:06:35 -0300

    Modificacion de impresion. Sin Cabecera y con detalle por concepto

--- src/components/pages/siaru/Liquidaciones/Impresion/CabeceraPDF.js
+++ src/components/pages/siaru/Liquidaciones/Impresion/CabeceraPDF.js
@@ -3,78 +3,78 @@ import { Document, Page, Text, View, Image } from "@react-pdf/renderer";
 import JsBarcode from "jsbarcode";
 import logo1 from "media/Logo1_sidebar.png";
 import AsArray from "components/helpers/AsArray.js";
-import Descriptor from "components/helpers/Descriptor";
+// import Descriptor from "components/helpers/Descriptor";
 import Formato from "components/helpers/Formato.js";
 import Grid from "components/ui/Grid/Grid.js";
 import styles from "components/pages/siaru/liquidaciones/impresion/PDF.styles.js";
 
 const CabeceraPDF = ({ data = {} } = {}) => {
-	const { empresa = {}, liquidaciones = [], ...cabecera } = data;
-	const descriptor = new Descriptor({
-		entero: {
-			singular: "peso",
-			plural: "pesos",
-		},
-		separador: "con",
-		decimal: {
-			singular: "centavo",
-			plural: "centavos",
-		},
-	});
+	const { /*empresa = {},*/ tiposPago = []/*, ...cabecera*/, seccional = {} } = data;
+	// const descriptor = new Descriptor({
+	// 	entero: {
+	// 		singular: "peso",
+	// 		plural: "pesos",
+	// 	},
+	// 	separador: "con",
+	// 	decimal: {
+	// 		singular: "centavo",
+	// 		plural: "centavos",
+	// 	},
+	// });
 
 	const GridView = ({ style, ...p }) => (
 		<Grid render={View} style={{ ...style, ...styles.border }} {...p} />
 	);
 
-	const Casillas = ({
-		width = 12,
-		height = 14,
-		border = {
-			color: "black",
-			style: "solid",
-			size: 1,
-		},
-		cantidad = 32,
-		style,
-		...p
-	}) => {
-		if (cantidad < 1) cantidad = 1;
-		style = {
-			...style,
-			borderStyle: border.style,
-			borderColor: border.color,
-			borderLeft: `${border.size}px`,
-			borderBottom: `${border.size}px`,
-		};
-		cantidad -= 1;
-		const casillas = [];
-		for (let n = 0; n < cantidad; n++) {
-			casillas.push(
-				<Grid
-					render={View}
-					block
-					width={`${width}px`}
-					height={`${height}px`}
-					style={style}
-					{...p}
-				/>
-			);
-		}
-		casillas.push(
-			<Grid
-				render={View}
-				block
-				width={`${width + border.size}px`}
-				height={`${height}px`}
-				style={{
-					...style,
-					borderRight: `${border.size}px`,
-				}}
-				{...p}
-			/>
-		);
-		return casillas;
-	};
+	// const Casillas = ({
+	// 	width = 12,
+	// 	height = 14,
+	// 	border = {
+	// 		color: "black",
+	// 		style: "solid",
+	// 		size: 1,
+	// 	},
+	// 	cantidad = 32,
+	// 	style,
+	// 	...p
+	// }) => {
+	// 	if (cantidad < 1) cantidad = 1;
+	// 	style = {
+	// 		...style,
+	// 		borderStyle: border.style,
+	// 		borderColor: border.color,
+	// 		borderLeft: `${border.size}px`,
+	// 		borderBottom: `${border.size}px`,
+	// 	};
+	// 	cantidad -= 1;
+	// 	const casillas = [];
+	// 	for (let n = 0; n < cantidad; n++) {
+	// 		casillas.push(
+	// 			<Grid
+	// 				render={View}
+	// 				block
+	// 				width={`${width}px`}
+	// 				height={`${height}px`}
+	// 				style={style}
+	// 				{...p}
+	// 			/>
+	// 		);
+	// 	}
+	// 	casillas.push(
+	// 		<Grid
+	// 			render={View}
+	// 			block
+	// 			width={`${width + border.size}px`}
+	// 			height={`${height}px`}
+	// 			style={{
+	// 				...style,
+	// 				borderRight: `${border.size}px`,
+	// 			}}
+	// 			{...p}
+	// 		/>
+	// 	);
+	// 	return casillas;
+	// };
 
 	const Header = () => (
 		<GridView col gap="10px">
@@ -96,7 +96,7 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 	const Footer = () => <></>;
 
 	const MyPage = ({ children, ...x }) => (
-		<Page style={styles.page} size="A4" {...x}>
+		<Page style={styles.page} size="A5" orientation="landscape" {...x}>
 			<GridView col grow>
 				<Header />
 				<GridView col grow gap="10px">
@@ -109,158 +109,159 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 
 	const pages = [];
 	//#region resumen
-	pages.push(
-		<MyPage>
-			<GridView col style={styles.borderBox}>
-				<GridView>
-					<GridView
-						gap="10px"
-						style={{ ...styles.paddingBox, ...styles.borderRight }}
-					>
-						<GridView col width="20%">
-							<GridView justify="center">
-								<Text style={styles.titulo}>C.U.I.T.</Text>
-							</GridView>
-							<GridView justify="center">
-								<Text>{Formato.Cuit(empresa.cuit)}</Text>
-							</GridView>
-						</GridView>
-						<GridView col width="55%">
-							<GridView>
-								<Text style={styles.titulo}>Razón Social</Text>
-							</GridView>
-							<GridView>
-								<Text>{empresa.razonSocial}</Text>
-							</GridView>
-						</GridView>
-					</GridView>
-					<GridView col width="25%" style={{ ...styles.paddingBox }}>
-						<GridView justify="center">
-							<Text style={styles.titulo}>
-								{["Periodo (Año-Mes)", "Número de Acta"].at(
-									cabecera.tipoLiquidacion
-								)}
-							</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>
-								{[Formato.Periodo(cabecera.periodo), cabecera.acta].at(
-									cabecera.tipoLiquidacion
-								)}
-							</Text>
-						</GridView>
-					</GridView>
-				</GridView>
-				<GridView style={styles.borderTop}>
-					<GridView
-						col
-						width
-						style={{ ...styles.paddingBox, ...styles.borderRight }}
-					>
-						<GridView justify="center">
-							<Text style={styles.titulo}>Trabajadores</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>{cabecera.cantidadTrabajadores ?? 0}</Text>
-						</GridView>
-					</GridView>
-					<GridView col width style={{ ...styles.paddingBox }}>
-						<GridView justify="center">
-							<Text style={styles.titulo}>Remuneraciones</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>{Formato.Moneda(cabecera.totalRemuneraciones ?? 0)}</Text>
-						</GridView>
-					</GridView>
-				</GridView>
-				<GridView style={styles.borderTop}>
-					<GridView
-						col
-						width
-						style={{ ...styles.paddingBox, ...styles.borderRight }}
-					>
-						<GridView justify="center">
-							<Text style={styles.titulo}>Capital</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>{Formato.Moneda(cabecera.totalAporte ?? 0)}</Text>
-						</GridView>
-					</GridView>
-					<GridView
-						col
-						width
-						style={{ ...styles.paddingBox, ...styles.borderRight }}
-					>
-						<GridView justify="center">
-							<Text style={styles.titulo}>Intereses</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>{Formato.Moneda(cabecera.totalIntereses ?? 0)}</Text>
-						</GridView>
-					</GridView>
-					<GridView col width style={styles.paddingBox}>
-						<GridView justify="center">
-							<Text style={styles.titulo}>Total</Text>
-						</GridView>
-						<GridView justify="center">
-							<Text>{Formato.Moneda(cabecera.totalImporte ?? 0)}</Text>
-						</GridView>
-					</GridView>
-				</GridView>
-				<GridView style={styles.borderTop}>
-					<GridView width>
-						<GridView
-							col
-							width
-							style={{ ...styles.paddingBox, ...styles.borderRight }}
-						>
-							<GridView justify="center">
-								<Text style={styles.titulo}>Total sindical</Text>
-							</GridView>
-							<GridView justify="center">
-								<Text>{Formato.Moneda(cabecera.totalSindical ?? 0)}</Text>
-							</GridView>
-						</GridView>
-					</GridView>
-					<GridView width>
-						<GridView col width style={{ ...styles.paddingBox }}>
-							<GridView justify="center">
-								<Text style={styles.titulo}>Total solidario</Text>
-							</GridView>
-							<GridView justify="center">
-								<Text>{Formato.Moneda(cabecera.totalSolidario ?? 0)}</Text>
-							</GridView>
-						</GridView>
-					</GridView>
-				</GridView>
-			</GridView>
-		</MyPage>
-	);
+	// pages.push(
+	// 	<MyPage>
+	// 		<GridView col style={styles.borderBox}>
+	// 			<GridView>
+	// 				<GridView
+	// 					gap="10px"
+	// 					style={{ ...styles.paddingBox, ...styles.borderRight }}
+	// 				>
+	// 					<GridView col width="20%">
+	// 						<GridView justify="center">
+	// 							<Text style={styles.titulo}>C.U.I.T.</Text>
+	// 						</GridView>
+	// 						<GridView justify="center">
+	// 							<Text>{Formato.Cuit(empresa.cuit)}</Text>
+	// 						</GridView>
+	// 					</GridView>
+	// 					<GridView col width="55%">
+	// 						<GridView>
+	// 							<Text style={styles.titulo}>Razón Social</Text>
+	// 						</GridView>
+	// 						<GridView>
+	// 							<Text>{empresa.razonSocial}</Text>
+	// 						</GridView>
+	// 					</GridView>
+	// 				</GridView>
+	// 				<GridView col width="25%" style={{ ...styles.paddingBox }}>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>
+	// 							{["Periodo (Año-Mes)", "Número de Acta"].at(
+	// 								cabecera.tipoLiquidacion
+	// 							)}
+	// 						</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>
+	// 							{[Formato.Periodo(cabecera.periodo), cabecera.acta].at(
+	// 								cabecera.tipoLiquidacion
+	// 							)}
+	// 						</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 			</GridView>
+	// 			<GridView style={styles.borderTop}>
+	// 				<GridView
+	// 					col
+	// 					width
+	// 					style={{ ...styles.paddingBox, ...styles.borderRight }}
+	// 				>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>Trabajadores</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>{cabecera.cantidadTrabajadores ?? 0}</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 				<GridView col width style={{ ...styles.paddingBox }}>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>Remuneraciones</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>{Formato.Moneda(cabecera.totalRemuneraciones ?? 0)}</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 			</GridView>
+	// 			<GridView style={styles.borderTop}>
+	// 				<GridView
+	// 					col
+	// 					width
+	// 					style={{ ...styles.paddingBox, ...styles.borderRight }}
+	// 				>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>Capital</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>{Formato.Moneda(cabecera.totalAporte ?? 0)}</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 				<GridView
+	// 					col
+	// 					width
+	// 					style={{ ...styles.paddingBox, ...styles.borderRight }}
+	// 				>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>Intereses</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>{Formato.Moneda(cabecera.totalIntereses ?? 0)}</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 				<GridView col width style={styles.paddingBox}>
+	// 					<GridView justify="center">
+	// 						<Text style={styles.titulo}>Total</Text>
+	// 					</GridView>
+	// 					<GridView justify="center">
+	// 						<Text>{Formato.Moneda(cabecera.totalImporte ?? 0)}</Text>
+	// 					</GridView>
+	// 				</GridView>
+	// 			</GridView>
+	// 			<GridView style={styles.borderTop}>
+	// 				<GridView width>
+	// 					<GridView
+	// 						col
+	// 						width
+	// 						style={{ ...styles.paddingBox, ...styles.borderRight }}
+	// 					>
+	// 						<GridView justify="center">
+	// 							<Text style={styles.titulo}>Total sindical</Text>
+	// 						</GridView>
+	// 						<GridView justify="center">
+	// 							<Text>{Formato.Moneda(cabecera.totalSindical ?? 0)}</Text>
+	// 						</GridView>
+	// 					</GridView>
+	// 				</GridView>
+	// 				<GridView width>
+	// 					<GridView col width style={{ ...styles.paddingBox }}>
+	// 						<GridView justify="center">
+	// 							<Text style={styles.titulo}>Total solidario</Text>
+	// 						</GridView>
+	// 						<GridView justify="center">
+	// 							<Text>{Formato.Moneda(cabecera.totalSolidario ?? 0)}</Text>
+	// 						</GridView>
+	// 					</GridView>
+	// 				</GridView>
+	// 			</GridView>
+	// 		</GridView>
+	// 	</MyPage>
+	// );
 	//#endregion
 
-	//#region liquidaciones-detalle
+	//#region detalles
 	let canvas = document.createElement("canvas");
 	pages.push(
-		...AsArray(liquidaciones).map(
+		...AsArray(tiposPago).map(
 			({
-				establecimiento = {},
-				tipoPago = {},
-				codigoBarra = "",
-				...liquidacion
+				// establecimiento = {},
+				// tipoPago = {},
+				// codigoBarra = "",
+				// ...liquidacion
+				codigoBarra,
+				...tipoPago
 			}) => {
 				let barcode;
 				if (codigoBarra) {
 					JsBarcode(canvas, codigoBarra);
 					barcode = canvas.toDataURL();
 				}
-
 				if (barcode) barcode = <Image src={barcode} />;
 
 				return (
 					<MyPage>
 						<GridView col style={styles.borderBox}>
 							<GridView>
-								<GridView
+								{/* <GridView
 									gap="10px"
 									style={{ ...styles.paddingBox, ...styles.borderRight }}
 								>
@@ -280,16 +281,28 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 											<Text>{establecimiento.nombre}</Text>
 										</GridView>
 									</GridView>
-								</GridView>
-								<GridView col width="25%" style={{ ...styles.paddingBox }}>
+								</GridView> */}
+								<GridView
+									col
+									width="25%"
+									style={{ ...styles.paddingBox, ...styles.borderRight }}
+								>
 									<GridView justify="center">
-										<Text style={styles.titulo}>
-											Tipo de pago
-										</Text>
+										<Text style={styles.titulo}>Tipo de pago</Text>
+									</GridView>
+									<GridView justify="center">
+										<Text>{tipoPago.descripcion}</Text>
+									</GridView>
+								</GridView>
+								<GridView col width style={styles.paddingBox}>
+									<GridView justify="center">
+										<Text style={styles.titulo}>Seccional</Text>
 									</GridView>
 									<GridView justify="center">
 										<Text>
-											{tipoPago.descripcion}
+											{[seccional.codigo, seccional.descripcion]
+												.filter((r) => r)
+												.join(" - ")}
 										</Text>
 									</GridView>
 								</GridView>
@@ -304,15 +317,15 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 										<Text style={styles.titulo}>Trabajadores</Text>
 									</GridView>
 									<GridView justify="center">
-										<Text>{liquidacion.cantidadTrabajadores ?? 0}</Text>
+										<Text>{tipoPago.trabajadores ?? 0}</Text>
 									</GridView>
 								</GridView>
-								<GridView col width style={{ ...styles.paddingBox }}>
+								<GridView col width style={styles.paddingBox}>
 									<GridView justify="center">
 										<Text style={styles.titulo}>Remuneraciones</Text>
 									</GridView>
 									<GridView justify="center">
-										<Text>{Formato.Moneda(liquidacion.totalRemuneraciones ?? 0)}</Text>
+										<Text>{Formato.Moneda(tipoPago.remuneraciones ?? 0)}</Text>
 									</GridView>
 								</GridView>
 							</GridView>
@@ -326,7 +339,7 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 										<Text style={styles.titulo}>Capital</Text>
 									</GridView>
 									<GridView justify="center">
-										<Text>{Formato.Moneda(liquidacion.interesNeto ?? 0)}</Text>
+										<Text>{Formato.Moneda(tipoPago.capital ?? 0)}</Text>
 									</GridView>
 								</GridView>
 								<GridView
@@ -338,7 +351,7 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 										<Text style={styles.titulo}>Intereses</Text>
 									</GridView>
 									<GridView justify="center">
-										<Text>{Formato.Moneda(liquidacion.interesImporte ?? 0)}</Text>
+										<Text>{Formato.Moneda(tipoPago.intereses ?? 0)}</Text>
 									</GridView>
 								</GridView>
 								<GridView col width style={styles.paddingBox}>
@@ -346,12 +359,12 @@ const CabeceraPDF = ({ data = {} } = {}) => {
 										<Text style={styles.titulo}>Total</Text>
 									</GridView>
 									<GridView justify="center">
-										<Text>{Formato.Moneda(liquidacion.importeTotal ?? 0)}</Text>
+										<Text>{Formato.Moneda(tipoPago.total ?? 0)}</Text>
 									</GridView>
 								</GridView>
 							</GridView>
 						</GridView>
-						<GridView grow/>
+						<GridView grow />
 						<GridView>{barcode}</GridView>
 					</MyPage>
 				);
--- src/components/pages/siaru/Liquidaciones/Impresion/CabeceraPrint.js
+++ src/components/pages/siaru/Liquidaciones/Impresion/CabeceraPrint.js
@@ -4,6 +4,8 @@ import AsArray from "components/helpers/AsArray";
 import Round from "components/helpers/Round";
 import useQueryQueue from "components/hooks/useQueryQueue";
 import CabeceraPDF from "./CabeceraPDF";
+import Formato from "components/helpers/Formato";
+import dayjs from "dayjs";
 
 const CabeceraPrint = ({ data }) => {
 	//#region Trato queries a APIs
@@ -47,15 +49,26 @@ const CabeceraPrint = ({ data }) => {
 					},
 				};
 			}
-			case "GetBarcode": {
+			// case "GetBarcode": {
+			// 	return {
+			// 		config: {
+			// 			baseURL: "SIARU",
+			// 			endpoint: `/Liquidaciones/GenerarCodigoBarras`,
+			// 			method: "PATCH",
+			// 			okType: "text",
+			// 		},
+			// 	};
+			// }
+			case "GetSeccional": {
+				const { id, ...others } = params;
 				return {
 					config: {
-						baseURL: "SIARU",
-						endpoint: `/Liquidaciones/GenerarCodigoBarras`,
-						method: "PATCH",
-						okType: "text",
+						baseURL: "Afiliaciones",
+						endpoint: `/Seccional/${id}`,
+						method: "GET",
 					},
-				};
+					params: others,
+				}
 			}
 			default:
 				return null;
@@ -69,19 +82,31 @@ const CabeceraPrint = ({ data }) => {
 		parametros: {
 			LiquidacionTipoPagoIdSindical: 0,
 			LiquidacionTipoPagoIdSolidario: 0,
+			SeccionalIdPorDefecto: 0,
 		},
 		cuit: data.empresaCUIT,
 		establecimientos: AsArray(data?.liquidaciones)
 			.map((r) => r.empresaEstablecimientoId)
 			.filter((v, i, a) => v && a.indexOf(v) === i),
 		liquidaciones: AsArray(data?.liquidaciones).map(({ id }) => id),
-		tiposPago: AsArray(data?.liquidaciones).map(({tipoPagoId}) => tipoPagoId).filter(((v, i, a) => a.indexOf(v) === i)),
+		// barras: {
+		// 	convenio: "0080",
+		// 	tipoLiquidacion: `${data.tipoLiquidacion ?? 0}`,
+		// 	periodoActa: Formato.Mascara((data.tipoLiquidacion ?? 0) === 0 ? data.periodo ?? 0 : data.acta ?? 0, "######"),
+		// 	cuit: Formato.Mascara(data.empresaCUIT, "###########"),
+		// 	fechaVencimiento: dayjs(data.fechaVencimiento)?.format("YYYYMMDD") ?? "00000000",
+		// 	nroBoleta: Formato.Mascara(data.id, "########"),
+		// },
+		tiposPago: AsArray(data?.liquidaciones)
+			.map(({ liquidacionTipoPagoId }) => liquidacionTipoPagoId)
+			.filter((v, i, a) => a.indexOf(v) === i),
 		data: {
 			parametros: null,
 			empresa: null,
 			establecimientos: null,
 			tiposPago: null,
-			codigosBarra: null,
+			// codigosBarra: null,
+			seccional: null,
 		},
 		errors: null,
 	});
@@ -104,6 +129,22 @@ const CabeceraPrint = ({ data }) => {
 			if (Object.keys(changes.data).filter((k) => !changes.data[k]).length)
 				return;
 			setDependencias((o) => ({ ...o, ...changes }));
+		};
+		const onLoadParam = (paramName) => {
+			switch (paramName) {
+				case "SeccionalIdPorDefecto": {
+					if (changes.data.seccional) return;
+					pushQuery({
+						action: "GetSeccional",
+						params: { id: changes.data.parametros[paramName] },
+						onOk: async (seccional) => setData({ seccional }),
+						onError: async (error) => setData({ seccional: {} }, error),
+						onFinally: async () => applyChanges(),
+					});
+				}
+				default:
+					return;
+			}
 		};
 		if (!changes.data.parametros) {
 			const parametros = {};
@@ -132,6 +173,7 @@ const CabeceraPrint = ({ data }) => {
 						)
 							return;
 						setData({ parametros }, errores.length ? errores : null);
+						onLoadParam(paramName);
 					},
 				});
 			});
@@ -174,32 +216,40 @@ const CabeceraPrint = ({ data }) => {
 		if (!changes.data.tiposPago) {
 			pushQuery({
 				action: "GetLiquidacionesTiposPagos",
-				onOk: async (tiposPago) => setData({ tiposPago }),
+				onOk: async (tiposPagoList) =>
+					setData({
+						tiposPago: dependencias.tiposPago.map(
+							(tipoPagoId) =>
+								AsArray(tiposPagoList).find(({ id }) => id === tipoPagoId) ?? {
+									id: tipoPagoId,
+								}
+						),
+					}),
 				onError: async (error) => setData({ tiposPago: [] }, error),
 				onFinally: async () => applyChanges(),
 			});
 		}
-		if (!changes.data.codigosBarra) {
-			const codigosBarra = [];
-			const errores = [];
-			dependencias.liquidaciones.forEach((id) => {
-				pushQuery({
-					action: "GetBarcode",
-					params: { liquidacionId: id },
-					onOk: async (codigoBarra) => codigosBarra.push({ id, codigoBarra }),
-					onError: async (error) => errores.push(error),
-					onFinally: async () => {
-						if (
-							dependencias.liquidaciones.length !=
-							codigosBarra.length + errores.length
-						)
-							return;
-						setData({ codigosBarra }, errores.length ? errores : null);
-						applyChanges();
-					},
-				});
-			});
-		}
+		// if (!changes.data.codigosBarra) {
+		// 	const codigosBarra = [];
+		// 	const errores = [];
+		// 	dependencias.tiposPago.forEach((id) => {
+		// 		// pushQuery({
+		// 		// 	action: "GetBarcode",
+		// 		// 	params: { id: data.id, tiposPagoId: id },
+		// 		// 	onOk: async (codigoBarra) => codigosBarra.push({ id, codigoBarra }),
+		// 		// 	onError: async (error) => errores.push(error),
+		// 		// 	onFinally: async () => {
+		// 		// 		if (
+		// 		// 			dependencias.tiposPago.length !=
+		// 		// 			codigosBarra.length + errores.length
+		// 		// 		)
+		// 		// 			return;
+		// 		// 		setData({ codigosBarra }, errores.length ? errores : null);
+		// 		// 		applyChanges();
+		// 		// 	},
+		// 		// });
+		// 	});
+		// }
 	}, [dependencias, pushQuery]);
 	//#endregion
 
@@ -207,52 +257,102 @@ const CabeceraPrint = ({ data }) => {
 
 	if (dependencias.errors) console.error({ errors: dependencias.errors });
 
-	const { empresa, establecimientos, tiposPago, parametros, codigosBarra } =
+	const { empresa, tiposPago, seccional/*, establecimientos, codigosBarra, parametros*/ } =
 		dependencias.data;
 
-	const { totalAporte, totalIntereses } = data;
-	data.totalImporte = Round(totalAporte + totalIntereses, 2)
+	// const { totalAporte, totalIntereses } = data;
+	// data.totalImporte = Round(totalAporte + totalIntereses, 2)
 
-	data.totalSindical = 0;
-	data.totalSolidario = 0;
+	// data.totalSindical = 0;
+	// data.totalSolidario = 0;
 	const liquidaciones = AsArray(data.liquidaciones);
-	liquidaciones.forEach((liquidacion) => {
-		liquidacion.establecimiento =
-			establecimientos.find(
-				({ id }) => id === liquidacion.empresaEstablecimientoId
-			) ?? {};
+	// liquidaciones.forEach((liquidacion) => {
+	// 	liquidacion.establecimiento =
+	// 		establecimientos.find(
+	// 			({ id }) => id === liquidacion.empresaEstablecimientoId
+	// 		) ?? {};
 
-		liquidacion.tipoPago =
-			tiposPago.find(({ id }) => id === liquidacion.liquidacionTipoPagoId) ??
-			{};
+	// 	liquidacion.tipoPago =
+	// 		tiposPago.find(({ id }) => id === liquidacion.liquidacionTipoPagoId) ??
+	// 		{};
 
-		const { interesImporte = 0, interesNeto = 0 } = liquidacion;
-		const { LiquidacionTipoPagoIdSindical, LiquidacionTipoPagoIdSolidario } =
-			parametros;
+	// 	const { interesImporte = 0, interesNeto = 0 } = liquidacion;
+	// 	const { LiquidacionTipoPagoIdSindical, LiquidacionTipoPagoIdSolidario } =
+	// 		parametros;
 
-		liquidacion.importeTotal = Round(interesImporte + interesNeto, 2);
-		liquidacion.codigoBarra =
-			codigosBarra.find(({ id }) => id === liquidacion.id)?.codigoBarra ?? "";
+	// 	liquidacion.importeTotal = Round(interesImporte + interesNeto, 2);
+	// 	liquidacion.codigoBarra =
+	// 		codigosBarra.find(({ id }) => id === liquidacion.id)?.codigoBarra ?? "";
 
-		switch (liquidacion.liquidacionTipoPagoId) {
-			case LiquidacionTipoPagoIdSindical: {
-				data.totalSindical += liquidacion.importeTotal;
-				break;
-			}
-			case LiquidacionTipoPagoIdSolidario: {
-				data.totalSolidario += liquidacion.importeTotal;
-				break;
-			}
-			default: {
-				break;
-			}
-		}
+	// 	switch (liquidacion.liquidacionTipoPagoId) {
+	// 		case LiquidacionTipoPagoIdSindical: {
+	// 			data.totalSindical += liquidacion.importeTotal;
+	// 			break;
+	// 		}
+	// 		case LiquidacionTipoPagoIdSolidario: {
+	// 			data.totalSolidario += liquidacion.importeTotal;
+	// 			break;
+	// 		}
+	// 		default: {
+	// 			break;
+	// 		}
+	// 	}
+	// });
+	// data.totalSindical = Round(data.totalSindical, 2);
+	// data.totalSolidario = Round(data.totalSolidario, 2);
+
+	tiposPago.forEach((tipoPago) => {
+		tipoPago.trabajadores = 0;
+		tipoPago.remuneraciones = 0;
+		tipoPago.capital = 0;
+		tipoPago.intereses = 0;
+		tipoPago.total = 0;
+		liquidaciones
+			.filter(
+				({ liquidacionTipoPagoId }) => liquidacionTipoPagoId === tipoPago.id
+			)
+			.forEach((liquidacion) => {
+				tipoPago.trabajadores += liquidacion.cantidadTrabajadores ?? 0;
+				tipoPago.remuneraciones += liquidacion.totalRemuneraciones ?? 0;
+				tipoPago.capital += liquidacion.interesNeto ?? 0;
+				tipoPago.intereses += liquidacion.interesImporte ?? 0;
+			});
+		tipoPago.remuneraciones = Round(tipoPago.remuneraciones, 2);
+		tipoPago.capital = Round(tipoPago.capital, 2);
+		tipoPago.intereses = Round(tipoPago.intereses, 2);
+		tipoPago.total = Round(tipoPago.capital + tipoPago.intereses, 2);
+		tipoPago.codigoBarra = (({
+			convenio = "0080",
+			concepto = `${tipoPago.id ?? 0}`[0],
+			tipoLiquidacion = data.tipoLiquidacion ?? 0,
+			periodoActa = Formato.Mascara(
+				(data.tipoLiquidacion ?? 0) === 0 ? data.periodo ?? 0 : data.acta ?? 0,
+				"######"
+			),
+			cuit = Formato.Mascara(data.empresaCUIT, "###########"),
+			fechVto = dayjs(data.fechaVencimiento)?.format("YYYYMMDD") ?? "00000000",
+			nroBoleta = Formato.Mascara(data.id, "########"),
+			totalPagar = Formato.Mascara(Round(tipoPago.total * 100, 0), "#########"),
+			cantTrab = Formato.Mascara(tipoPago.trabajadores, "#####"),
+			secc = `${seccional.codigo ?? ""}`.slice(1),
+			digitoVerif = "0",
+			completa = "0".repeat(22),
+		} = {}) =>
+			convenio +
+			concepto +
+			tipoLiquidacion +
+			periodoActa +
+			cuit +
+			fechVto +
+			nroBoleta +
+			totalPagar +
+			cantTrab +
+			secc +
+			digitoVerif +
+			completa)();
 	});
-	data.totalSindical = Round(data.totalSindical, 2);
-	data.totalSolidario = Round(data.totalSolidario, 2);
 
-	const cabecera = { ...data, empresa, liquidaciones };
-	console.log({ cabecera });
+	const cabecera = { ...data, empresa, tiposPago, seccional };
 
 	return (
 		<PDFViewer style={{ flexGrow: 1 }}>
