476a09685d131dec4c4c879d85843a538afb6a0a
Rodrigo Lopez <rodrigo@binarianet.com>
2024-01-17 09:04:50 -0300

    Pantalla inicial a mantenimiento de localidades

---
+++ src/components/pages/administracion/localidades/LocalidadesHandler.js
@@ -0,0 +1,13 @@
+import Grid from "components/ui/Grid/Grid";
+import React from "react";
+
+const LocalidadesHandler = () => {
+	return (
+		<Grid full col>
+			<Grid className="titulo"><h1>Localidades</h1></Grid>
+			<Grid className="tabs">Contenido</Grid>
+		</Grid>
+	);
+};
+
+export default LocalidadesHandler;
