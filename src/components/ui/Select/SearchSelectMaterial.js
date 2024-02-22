41498ddb3f4074f2544f4a02923e14b489283a8c
Rodrigo Lopez <rodrigo@binarianet.com>
2024-02-22 09:03:03 -0300

    Funciones utiles SearchSelectMaterial

--- src/components/ui/Select/SearchSelectMaterial.js
+++ src/components/ui/Select/SearchSelectMaterial.js
@@ -3,6 +3,69 @@ import { FormControl, Autocomplete } from "@mui/material";
 import InputMaterial from "../Input/InputMaterial";
 import styles from "./SearchSelectMaterial.module.css";
 
+const optionDef = { value: null, label: null };
+const optionArray = [optionDef];
+
+/**
+ * @param {*} value
+ * @param {number} index
+ * @param {array} array
+ */
+const mapDef = (value, index, array) => ({ value, label: value });
+
+/**
+ * @param {optionDef} option
+ */
+const filterDef = (option) => !!option;
+
+/**
+ * Generador de opciones a partir de datos crudos
+ * @param {object} config Configuración
+ * @param {array} config.data Datos origen
+ * @param {mapDef} config.map Funcion que convierte un dato a una opcion
+ * @param {optionArray} config.start Opciones a agregar al inicio
+ * @param {optionArray} config.end Opciones a agregar al final
+ * @param {filterDef} config.filter Filtro a aplicar a las opciones
+ * @returns Arreglo de opciones que cumplen con filter
+ */
+export const mapOptions = ({
+	data,
+	map = mapDef,
+	start = [],
+	end = [],
+	filter = filterDef,
+}) => [...start, ...data.map(map), ...end].filter(filter);
+
+/**
+ * Convierte value a minúsculas o mayúsculas dependiendo de casing
+ * @param {string} value Valor a capitalizar
+ * @param {number} casing
+ * Si mayor a 0, convierte value a mayúsculas.
+ * 
+ * Si menor a 0, convierte value a minúsculas.
+ * 
+ * Si 0, no aplica conversión.
+ * 
+ * Por defecto es 0.
+ * @returns Alteración de value según casing
+ */
+export const getCase = (value, casing = 0) =>
+	casing < 0 ? value.toLowerCase() : casing > 0 ? value.toUpperCase() : value;
+
+/**
+ * Condición si option incluye search en su label
+ * @param {optionDef} option Opcion a evaluar
+ * @param {string} search Valor a buscar en label de option
+ * @param {boolean} ignoreCase
+ * Si debe o no considerar mayúsculas y minúsculas.
+ * 
+ * Por defecto no considera.
+ * @returns true o false dependiendo de si search tiene valor y cumple la condición.
+ */
+export const includeSearch = (option, search, ignoreCase = true) =>
+	!search ||
+	getCase(option.label, ignoreCase).includes(getCase(search, ignoreCase));
+
 // const ITEM_HEIGHT = 48;
 // const ITEM_PADDING_TOP = 8;
 
