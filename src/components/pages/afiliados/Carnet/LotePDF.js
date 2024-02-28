abd61665e1b5c2c7cb1ad3b9c4e6461a8696ac89
Rodrigo Lopez <rodrigo@binarianet.com>
2024-02-28 16:55:12 -0300

    Ajustes varios a la impresión de carnet

--- src/components/pages/afiliados/Carnet/LotePDF.js
+++ src/components/pages/afiliados/Carnet/LotePDF.js
@@ -1,17 +1,24 @@
 import React from "react";
 import JsBarcode from "jsbarcode";
-import { Document, Page, Text, View, Image, StyleSheet } from "@react-pdf/renderer";
+import {
+	Document,
+	Page,
+	Text,
+	View,
+	Image,
+	StyleSheet,
+} from "@react-pdf/renderer";
 import UIGrid from "components/ui/Grid/Grid";
 import Formato from "components/helpers/Formato.js";
 
-const fontSizePt = 8
+const fontSizePt = 8;
 const styles = StyleSheet.create({
 	document: { flexGrow: 1 },
 	page: {
-		paddingLeft: 20,
-		paddingRight: 20,
-		paddingTop: 20,
-		paddingBottom: 20,
+		paddingLeft: 5,
+		paddingRight: 5,
+		paddingTop: 5,
+		paddingBottom: 5,
 		display: "flex",
 		position: "absolute",
 		left: 0,
@@ -35,7 +42,13 @@ const Grid = ({ style, ...p }) => (
 );
 
 /** @type {Text} */
-const P = ({ size = (fontSizePt - 2), align = "left", bold = false, style, ...p }) => (
+const P = ({
+	size = fontSizePt - 2,
+	align = "left",
+	bold = false,
+	style,
+	...p
+}) => (
 	<Text
 		style={{
 			fontSize: `${size < 0 ? 1 : size}pt`,
@@ -50,6 +63,8 @@ const P = ({ size = (fontSizePt - 2), align = "left", bold = false, style, ...p 
 // 54mm x 86mm
 const CR80 = [153.07, 243.78];
 
+const canvas = document.createElement("canvas");
+
 const Hoja = ({
 	nombre = "",
 	tipoDocumento = "",
@@ -61,60 +76,55 @@ const Hoja = ({
 	seccional = "",
 	fechaIngreso = "",
 	cuil = "",
-	barcodeDataURL = "",
-}) => (
-	<Page style={styles.page} size={CR80} orientation="landscape">
-		<Grid col full gap="5px">
-			<P size={fontSizePt + 2} align="center" bold>
-				{nombre}
-			</P>
-			<Grid width="full">
-				<Grid col width="full" gap="2px">
-					<P align="center">
-						{[tipoDocumento, Formato.DNI(documento)].filter((r) => r).join(" ")}
-					</P>
-					<P align="center">{`Afil. Nro. ${nroAfiliado}`}</P>
-					<P size={fontSizePt} align="center" bold>
-						{provincia}
-					</P>
+}) => {
+	JsBarcode(canvas, Formato.Mascara(cuil, "##-########-#T"), {
+		format: "CODE39",
+		height: "50px",
+		displayValue: false,
+	});
+	return (
+		<Page style={styles.page} size={CR80} orientation="landscape">
+			<Grid col width gap="2px">
+				<P size={fontSizePt + 2} align="center" bold>
+					{nombre}
+				</P>
+				<Grid width="full">
+					<Grid col width="full" gap="2px">
+						<P align="center">
+							{[tipoDocumento, Formato.Numero(documento)]
+								.filter((r) => r)
+								.join(" ")}
+						</P>
+						<P align="center">{`Afil. Nro. ${Formato.Numero(nroAfiliado)}`}</P>
+						<P size={fontSizePt} align="center" bold>
+							{provincia}
+						</P>
+					</Grid>
+					<Grid col width="full" gap="2px">
+						<P align="center">{nacionalidad}</P>
+						<P align="center">
+							{((v) => (v ? `Secc. Nro. ${v}` : ""))(seccionalCodigo)}
+						</P>
+						<P size={fontSizePt} align="center" bold>
+							{seccional}
+						</P>
+					</Grid>
 				</Grid>
-				<Grid col width="full" gap="2px">
-					<P align="center">{nacionalidad}</P>
-					<P align="center">
-						{((v) => (v ? `Secc. Nro. ${v}` : ""))(seccionalCodigo)}
-					</P>
-					<P size={fontSizePt} align="center" bold>
-						{seccional}
-					</P>
-				</Grid>
-			</Grid>
-			<P size={fontSizePt + 2} align="center" bold>
-				{`Emisión: ${Formato.Fecha(fechaIngreso)}`}
-			</P>
-			<Grid width="full" justify="center" grow>
-				<Grid col width="75%" justify="center">
-					<Image src={barcodeDataURL} />
-					<P size={fontSizePt} align="center">
-						{Formato.Mascara(cuil, "##-########-#")}
-					</P>
-				</Grid>
-			</Grid>
-			<Grid width="full">
-				<Grid col width="70%">
-					<P>ESTA TARJETA PERTENECE A UATRE</P>
-					<P>EN CASO DE ENCONTRARLA</P>
-					<P>AGRADECEMOS SE ENVIE A:</P>
-					<P>RECONQUISTA 630 PISO 6º - CIUDAD DE BS. AS.</P>
-					<P>TEL.: 011-315-5800</P>
-				</Grid>
-				<Grid col width="30%" justify="end">
-					<P align="center">Secretariado Nacional</P>
-					<P align="center">U.A.T.R.E.</P>
+				<P size={fontSizePt + 2} align="center" bold>
+					{`Emisión: ${Formato.Fecha(fechaIngreso)}`}
+				</P>
+				<Grid width="full" justify="center" grow>
+					<Grid col width="75%" justify="center">
+						<Image src={canvas.toDataURL()} />
+						<P size={fontSizePt} align="center">
+							{Formato.Mascara(cuil, "##-########-#")}
+						</P>
+					</Grid>
 				</Grid>
 			</Grid>
-		</Grid>
-	</Page>
-);
+		</Page>
+	);
+};
 
 /**
  * Impresion de credenciales de afiliados en lote.
@@ -123,17 +133,11 @@ const Hoja = ({
  * @param {array} props.data Datos de afiliados a imprimirles credenciales.
  */
 const LotePDF = ({ title = "Credenciales de afiliados", data }) => {
-	let canvas = document.createElement("canvas");
 	return (
 		<Document style={styles.document} title={title}>
-			{data.map((v, i) => {
-				JsBarcode(canvas, Formato.Mascara(v.cuil, "##-########-#T"), {
-					format: "CODE39",
-					height: "50px",
-					displayValue: false,
-				});
-				return <Hoja {...v} key={i} barcodeDataURL={canvas.toDataURL()} />;
-			})}
+			{data.map((v, i) => (
+				<Hoja {...v} key={i} />
+			))}
 		</Document>
 	);
 };
