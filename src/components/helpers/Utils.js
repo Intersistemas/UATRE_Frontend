7ed6115a47f948cb565434dce83944d6533c7444
Rodrigo Lopez <rodrigo@binarianet.com>
2024-02-29 19:26:39 -0300

    Agrego documentación y mejoro performance de iff y xor

--- src/components/helpers/Utils.js
+++ src/components/helpers/Utils.js
@@ -1,13 +1,82 @@
-/** !a */
-export const not = (a) => !(typeof a === "function" ? a() : a);
-/** Si todos los parámetros son iguales */
+/** 
+ * @returns {boolean} Identidad de a.
+ *
+ * Si a es una función, la ejecuta.
+ */
+export const id = (a) => !!(typeof a === "function" ? a() : a);
+
+/** Sobreescrive array[index] con la identidad de value
+ * @param {*} value
+ * @param {number} index
+ * @param {array} array
+ * @returns Identidad de value
+ * */
+const overrideId = (value, index, array) => {
+	if (typeof value !== "function") return value;
+	const r = id(value);
+	array[index] = r;
+	return r;
+};
+
+/** 
+ * @returns {boolean} Negación de a. (&not;a)
+ *
+ * Si a es una función, la ejecuta.
+ */
+export const not = (a) => !(typeof va === "function" ? a() : a);
+/** 
+ * @returns {boolean} Si todos los parámetros son iguales. (===)
+ * */
 export const eq = (a, ...b) => b.findIndex((r) => r !== a) === -1;
-/** Si se cumple algun parámetro */
-export const or = (...a) => a.findIndex((r) => !not(r)) !== -1;
-/** Si se cumplen todos los parámetros */
+/** 
+ * @returns {boolean} Si se cumple algun parámetro. (&or;)
+ *
+ * Si un parámetro es función, lo ejecuta.
+ * */
+export const or = (...a) => a.findIndex((r) => id(r)) !== -1;
+/** 
+ * @returns {boolean} Si se cumplen todos los parámetros. (&and;)
+ *
+ * Si un parámetro es función, lo ejecuta.
+ * */
 export const and = (...a) => a.findIndex((r) => not(r)) === -1;
-/** Si se cumple algun parámetro pero no todos */
-export const xor = (...a) => or(...a) && !and(...a);
+/**
+ * @returns {boolean} Si se cumple algun parámetro pero no todos. (&veebar;)
+ *
+ * Si un parámetro es función, lo ejecuta.
+ */
+export const xor = (...a) =>
+	// or(...a) && !and(...a); Lo hago de otra forma, para ejecutar los params fn solo una vez
+	a.findIndex(overrideId) !== -1 &&
+	a.findIndex((v, i, a) => !overrideId(v, i, a)) !== -1;
+/**
+ * @returns {boolean} Si se cumplen todos lo parámetros o ninguno. (&hArr;)
+ *
+ * Si un parámetro es función, lo ejecuta.
+ * */
+export const iff = (...a) =>
+	// and(...a) || !or(...a); Lo hago de otra forma, para ejecutar los params fn solo una vez
+	a.findIndex((v, i, a) => !overrideId(v, i, a)) === -1 ||
+	a.findIndex(overrideId) === -1;
+/**
+ * @returns {boolean} Si el primer parametro implica el resto. (&rArr;)
+ *
+ * Recursivo con mas de 2 parámetros.
+ *
+ * Si un parámetro es función, lo ejecuta.
+ * */
+export const then = (...a) => {
+	switch (a.length) {
+		case 0:
+			return true;
+		case 1:
+			return id(a[0]);
+		case 2:
+			return not(a[0]) || id(a[1]);
+		default:
+			return not(a.shift()) || then(...a);
+	}
+};
 
 /**
  * Compara a contra b.
