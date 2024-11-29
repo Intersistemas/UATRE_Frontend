c7ccfe450bbacab63a3dad22769622b6df179dba
Rodrigo J López <rodrilopez90@gmail.com>
2024-11-29 14:33:46 -0300

    Posibilidad de armar las urls de apis de tal forma que se pueda probar una en específico sin afectar a las demás

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -180,7 +180,7 @@ const Login = () => {
         <div className="mt-2">
           <a><Link to="/contacto"> <text className={classes.underline}>C</text>ontacto</Link></a>
         </div>  
-        {process.env.REACT_APP_URL_BASE.includes("http:") && <h6 style={{textShadow: '1px 1px 15px yellow'}} className="float-end">{process.env.REACT_APP_URL_BASE.toUpperCase()}</h6>}
+        {process.env.REACT_APP_PROTOCOL !== "https" && <h6 style={{textShadow: '1px 1px 15px yellow'}} className="float-end">{process.env.REACT_APP_SERVER.toUpperCase()}</h6>}
       
       </LoginCard>
      
