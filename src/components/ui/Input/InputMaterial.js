5f9ff5e3cd1d06c9adb320f2c77cc312bbb61151
Rodrigo J López <rodrilopez90@gmail.com>
2025-04-01 15:21:35 -0300

    Reimplemento InputMaterial para utilizar imask en lugar de react-text-mask

--- src/components/ui/Input/InputMaterial.js
+++ src/components/ui/Input/InputMaterial.js
@@ -1,15 +1,31 @@
-import MaskedInput from "react-text-mask";
-import { TextField } from "@mui/material";
-import { MuiTelInput } from "mui-tel-input";
-import { getType } from "components/helpers/Utils";
-import DateTimePicker from "../DateTimePicker/DateTimePicker";
+import { useMemo } from "react";
 import styles from "./InputMaterial.module.css";
+import { MuiTelInput } from "mui-tel-input";
+import { TextField } from "@mui/material";
+import { deepFreeze, getType } from "components/helpers/Utils";
+import DateTimePicker from "../DateTimePicker/DateTimePicker";
+import { MaskTextField } from "./MaskTextField";
 
-const onChangeDef = (value, id) => {};
+const onChangeDef = (value, id) => { };
 
-export const CUITMask = Object.freeze([/\d/, /\d/, "-", /\d/, /\d/, ".", /\d/, /\d/, /\d/,".",/\d/,/\d/,/\d/,"-",/\d/]);
-export const DNIMask = Object.freeze([/\d/, /\d/, ".", /\d/, /\d/, /\d/,".",/\d/,/\d/,/\d/]);
-export const CodSeccional = Object.freeze(['S', "-",/\d/,/\d/,/\d/,/\d/]);
+export const CUITMask = deepFreeze({ mask: "00-00.000.000-0", unmask: true });
+export const DNIMask = deepFreeze({ mask: Number, unmask: "typed", scale: 0, thousandsSeparator: ".", min: 0, max: 99999999 });
+export const CodSeccional = deepFreeze({ mask: "S-0000", unmask: false });
+export const PesosMask = deepFreeze({
+	mask: "$ d",
+	lazy: false,
+	blocks: {
+		d: {
+			mask: Number,
+			scale: 2,
+			thousandsSeparator: ".",
+			radix: ",",
+			mapToRadix: ["."],
+			expose: true,
+		},
+	},
+	unmask: true,
+});
 
 const InputMaterial = ({
 	id,
@@ -21,26 +37,40 @@ const InputMaterial = ({
 	onChange = onChangeDef,
 	...x
 }) => {
-	//console.log('InputMaterial_parametros ',x)
+	const state = useMemo(() => ({ id: id ?? `UUID${crypto.randomUUID()}` }), [id]);
+
+	const handleOnChange = (value) => {
+		switch (state.id) {
+			case "cuit":
+				const reCUIT = /^[0-9\b]+$/;
+				if (value !== "" && !reCUIT.test(value)) return;
+				break;
+			default:
+				break;
+		}
+		onChange(value, state.id);
+	}
+
 	const textFieldProps = {
+		...state,
 		className: styles.input,
-		id,
 		size,
 		...x,
 		style: { width: isNaN(width) ? width : `${width}%`, ...x.style },
 		InputLabelProps: { shrink: true, ...x.InputLabelProps },
 		InputProps: { readOnly, ...x.InputProps },
-		onChange: (v) => onChange(v, id),
+		onChange: handleOnChange,
 	};
 	textFieldProps.FormHelperTextProps ??= {};
 	textFieldProps.FormHelperTextProps.style = {
 		marginTop: "0px",
 		...textFieldProps.FormHelperTextProps.style,
 	};
-	
+
 	let subtype = null;
 	({ type, subtype } = getType(type));
 	textFieldProps.type = subtype ?? type;
+	textFieldProps.name ??= state.id;
 
 	switch (type) {
 		case "tel": {
@@ -59,36 +89,20 @@ const InputMaterial = ({
 		default:
 			break;
 	}
-	
-	if (id === "cuil" && !("autoFocus" in textFieldProps))
-		textFieldProps.autoFocus = true;
 
-	textFieldProps.onChange = ({ target }) => {
-		switch (id) {
-			case "cuit":
-				const reCUIT = /^[0-9\b]+$/;
-				if (target?.value === "" || reCUIT.test(target?.value)) {
-					onChange(target?.value, id);
-				}
-				break;
-			default:
-				onChange(target?.value, id);
-				break;
-		}
-	};
+	if (state.id === "cuil" && !("autoFocus" in textFieldProps))
+		textFieldProps.autoFocus = true;
 
 	textFieldProps.value ??= ""
 
 	if (mask) {
-		textFieldProps.mask = mask;
-		return (
-			<MaskedInput
-				{...textFieldProps}
-				guide={true}
-				render={(ref, props) => <TextField inputRef={ref} {...props} />}
-			/>
-		);
+		const { onChange: onAccept, ...mtfProps } = { ...mask, ...textFieldProps };
+		mtfProps.onAccept = onAccept;
+		mtfProps.value = `${textFieldProps.value}`; // value debe ser string
+		return <MaskTextField {...mtfProps} />;
 	}
+
+	textFieldProps.onChange = ({ target }) => handleOnChange(target?.value);
 
 	return <TextField {...textFieldProps} />;
 };
