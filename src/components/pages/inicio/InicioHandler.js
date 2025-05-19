5130f2055a571d5d5c3596e4157f1b6812fcab90
Alexis Prieto <prietoalexis99@gmail.com>
2025-05-19 17:00:00 -0300

    FIX ANUNCIO MODAL

--- src/components/pages/inicio/InicioHandler.js
+++ src/components/pages/inicio/InicioHandler.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Inicio from "./Inicio";
 import { handleModuloSeleccionar } from "../../../redux/actions";
+import AnuncioModal from "./AnuncioModal";
+import AuthContext from "store/authContext";
 
 const InicioHandler = () => {
   
     const dispatch = useDispatch();
     dispatch(handleModuloSeleccionar(""));
+   
 
     //Obtengo los modulos del usuario logueado  
     const usuarioLogueado = useSelector(
       (state) => state.usuarioLogueado
     );
     
+    const authContext = useContext(AuthContext)
+    const Usuario = authContext.usuario;
+
+    const [anuncio, setAnuncio] = React.useState(Usuario?.verAnuncio);
+
     let modulos = [];
 
     usuarioLogueado?.modulosTareas.forEach((mod) => {
       if (modulos.includes(mod.nombreModulo)) return;
       modulos.push(mod.nombreModulo);
     });
+
     
     return (
       <div>
         <Inicio modulos={modulos} tareas={usuarioLogueado?.modulosTareas} roles={usuarioLogueado?.roles}/>
+        {anuncio && <AnuncioModal onClose={() => setAnuncio(false)}/>}
       </div>
     );
 };
