e4c6d96fb42d53675779ccee0526f4891442c0e2
Alexis Prieto <prietoalexis99@gmail.com>
2025-06-09 13:22:43 -0300

    se habilita Visita a Seccional y se cambia el pdf

--- src/components/pages/consultas/ConsultasHandler.js
+++ src/components/pages/consultas/ConsultasHandler.js
@@ -114,7 +114,7 @@ const ConsultasHandler = () => {
 					</Button>
 				</Grid>
 
-				<Grid width gap="inherit" justify="evenly" display="none">
+				<Grid width gap="inherit" justify="evenly">
 					<Button
 						className="botonAmarillo"
 						onClick={onDownloadVisitaSeccional}
