7e08293df958f4de0d8cda0cea2665a34f3e7c45
Alexis Prieto <prietoalexis99@gmail.com>
2025-06-27 20:44:51 -0300

    ajustes solicitud afiliacion por empresa

--- src/components/pages/consultas/ConsultasHandler.js
+++ src/components/pages/consultas/ConsultasHandler.js
@@ -10,6 +10,7 @@ import SeccionalesMap from "./seccionalMaps/seccionalesMap";
 import useSolicitudAfiliacion from "./solicitudAfiliacion/SolicitudAfiliacion";
 import SolicitudAfiliacionForm from "./solicitudAfiliacion/SolicitudAfiliacionForm";
 import SolicitudAutorizacionAfiliacion from "./solicitudAfiliacion/AfiliacionesPorEmpresa/SolicitudAutorizacionAfiliacion";
+import AfiliacionesPorEmpresa from "./afiliacionesPorEmpresa/AfiliacionesPorEmpresaHandler";
 import AfiliadoFormulariosAfiliacionHandler from "./solicitudAfiliacion/AfiliadoFormulariosAfiliacionHandler";
 
 const ConsultasHandler = () => {
@@ -137,7 +138,7 @@ const ConsultasHandler = () => {
 					</Button>
 				
 				</Grid>
-				{/**Nuevo boton agregado */}
+				{/**Nuevo boton agregado 
 				<Grid width gap="inherit" justify="evenly">
 					<Button
 						className="botonAmarillo"
@@ -148,13 +149,25 @@ const ConsultasHandler = () => {
 							)
 						}
 						width="32"
+						hide={true}
+						//tarea="Informes_Afiliados_AfiliadosEmpresa"
+					>
+						Afiliaciones por Empresa 2
+					</Button>
+				
+				</Grid>*/}
+				<Grid width gap="inherit" justify="evenly">
+					<Button
+						className="botonAmarillo"
+						
+						onClick={() => navigate("AfiliacionesPorEmpresa")}
+						width="32"
 						//tarea="Informes_Afiliados_AfiliadosEmpresa"
 					>
 						Afiliaciones por Empresa
 					</Button>
 				
 				</Grid>
-				
 				
 			</>
 		),
--- src/components/pages/consultas/solicitudAfiliacion/AfiliacionesPorEmpresa/SolicitudAutorizacionAfiliacion.js
+++ src/components/pages/consultas/solicitudAfiliacion/AfiliacionesPorEmpresa/SolicitudAutorizacionAfiliacion.js
@@ -352,7 +352,7 @@ const SolicitudAutorizacionAfiliacion = ({
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${year}-${month}-${day}`;
-  };
+  }; 
 
   //-----------------------------------------------
   //#region filtros
