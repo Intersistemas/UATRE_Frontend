0916305ab7ee8f11b388d39bb09760a8bd5928c5
Alexis Prieto <prietoalexis99@gmail.com>
2025-08-05 13:50:56 -0300

    fix

--- src/components/pages/consultas/ConsultasHandler.js
+++ src/components/pages/consultas/ConsultasHandler.js
@@ -144,7 +144,7 @@ const ConsultasHandler = () => {
 							
 							onClick={() => navigate("Afiliaciones")}
 							width="32"
-							tarea="Consultas_AfiliadosEmpresa"
+							tarea="Consultas_AfiliacionesPorEmpresa"
 						>
 							Afiliaciones por Empresa
 						</Button>
