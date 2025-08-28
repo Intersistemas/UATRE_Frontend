11870e892ddfb1ccb26662da8f4a59622306f730
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-28 19:50:07 -0300

    Se soluciona el error de no poder modificar las observaciones

--- src/components/pages/gestionOS/GestionOSForm.js
+++ src/components/pages/gestionOS/GestionOSForm.js
@@ -38,9 +38,9 @@ import moment from "moment/moment";
 import useSolicitudAfiliacion from "../consultas/solicitudAfiliacion/SolicitudAfiliacion";
 import { useSelector } from "react-redux";
 
-const onChangeDef = (changes = {}) => {};
-const onCloseDef = (confirm = false) => {};
-const onValidateDef = (confirm = false) => {};
+const onChangeDef = (changes = {}) => { };
+const onCloseDef = (confirm = false) => { };
+const onValidateDef = (confirm = false) => { };
 
 /**
  * Proceso a ejecutar posterior carga
@@ -229,27 +229,22 @@ const GestionOSForm = ({
               "DD/MM/YYYY"
             )}</strong><br></br>` +
             `${data.gestionObraSocialDescripcion}<br></br>${data.gestionAreaOspreraDescripcion}<br></br><br></br>` +
-            `En representación del Afiliado <strong>${
-              !!data.titularPaciente
-                ? data?.apellidoTitular
-                : data?.apellidoPaciente
-            } ${
-              !!data.titularPaciente
-                ? data?.nombreTitular
-                : data?.nombrePaciente
-            }</strong>, con DNI Nº <strong>${
-              data?.dniPaciente ?? ""
-            }</strong>, Afiliado Nº <strong>${
-              data?.cuitTitular ?? ""
+            `En representación del Afiliado <strong>${!!data.titularPaciente
+              ? data?.apellidoTitular
+              : data?.apellidoPaciente
+            } ${!!data.titularPaciente
+              ? data?.nombreTitular
+              : data?.nombrePaciente
+            }</strong>, con DNI Nº <strong>${data?.dniPaciente ?? ""
+            }</strong>, Afiliado Nº <strong>${data?.cuitTitular ?? ""
             }</strong> ` +
             `se solicita <strong>${gestionRubroSelect?.selected?.label}</strong> sobre <strong>${gestionSubRubroSelect?.selected?.label}</strong> conforme lo que se detalla a continuación;<br></br>` +
             `<strong>${data?.texto}</strong>, adjuntando la documentación respectiva en su caso.<br><br/>` +
-            `Tipo de Adjuntos: <strong>${
-              documentacionList.length === 0
-                ? "Sin archivos adjuntos"
-                : documentacionList
-                    .map((a) => a.refTipoDocumentacionDescripcion)
-                    .join("/ ")
+            `Tipo de Adjuntos: <strong>${documentacionList.length === 0
+              ? "Sin archivos adjuntos"
+              : documentacionList
+                .map((a) => a.refTipoDocumentacionDescripcion)
+                .join("/ ")
             }</strong><br></br>` +
             `Se requiere que se brinde la misma a la mayor brevedad posible o se me indique al mail o teléfono que se detalla al pie los pasos a seguir al respecto.<br><br/>` +
             `La presente se origina por la imposibilidad del Afiliado de la referencia de realizarla por sus propios medios.<br><br/>` +
@@ -397,9 +392,9 @@ const GestionOSForm = ({
   useEffect(() => {
     if (
       data?.gestionRubroDescripcion?.toString().toLowerCase() ===
-        "uso app/wapp" ||
+      "uso app/wapp" ||
       data?.gestionRubroDescripcion?.toString().toLowerCase() ===
-        "registro de reclamos"
+      "registro de reclamos"
     ) {
       onChange({ medioGestion: "telefono" });
       setDisabledItems((o) => ({ ...o, medioGestion: true }));
@@ -492,12 +487,12 @@ const GestionOSForm = ({
     //if (request !== "A") return;
     conDatos
       ? solicitudAfiliacion({
-          data: dataFormulario,
-          onLoad: (base64) => download(base64, `SolicitudAfiliacion.pdf`),
-        })
+        data: dataFormulario,
+        onLoad: (base64) => download(base64, `SolicitudAfiliacion.pdf`),
+      })
       : solicitudAfiliacion({
-          onLoad: (base64) => download(base64, `SolicitudAfiliacion.pdf`),
-        });
+        onLoad: (base64) => download(base64, `SolicitudAfiliacion.pdf`),
+      });
   };
 
   const { setState: setDocumentosQuery } = useQueryState(
@@ -687,9 +682,9 @@ const GestionOSForm = ({
               Accept: "*/*",
             },
             /*body: JSON.stringify({
-							to: to,
-							attachments: attachments,
-						}),*/
+              to: to,
+              attachments: attachments,
+            }),*/
           },
         };
       }
@@ -1189,6 +1184,13 @@ const GestionOSForm = ({
   }, [setGestionObraSocialQuery]);
   //#endregion select GestionesObraSocial
 
+  // Habilitar Observaciones cuando se está en Modificar
+  useEffect(() => {
+    if (request === "M") {
+      setDisabledItems((prev) => ({ ...prev, observacionesEstado: false }));
+    }
+  }, [request]);
+
   useEffect(() => {
     const changes = {};
     if (data.telefono == null) changes.telefono = "+54 9";
@@ -1489,9 +1491,9 @@ const GestionOSForm = ({
           onDownloadSolicitudAfiliacion(true);
           setDialogTexto(
             "Se descargó la Solicitud de Afiliación de: " +
-              data?.apellidoTitular +
-              " " +
-              data?.nombreTitular
+            data?.apellidoTitular +
+            " " +
+            data?.nombreTitular
           );
           setOpenDialog(true);
         }
@@ -1649,13 +1651,13 @@ const GestionOSForm = ({
                           required
                           error={!!errors.cuitTitular}
                           /*helperText={
-											errors.cuitTitular ? errors.cuitTitular : validacionCUIT.validado
-										}
-										FormHelperTextProps={{
-											sx: {
-											margin: 0, // elimina el margen superior
-											},
-										}}*/
+                      errors.cuitTitular ? errors.cuitTitular : validacionCUIT.validado
+                    }
+                    FormHelperTextProps={{
+                      sx: {
+                      margin: 0, // elimina el margen superior
+                      },
+                    }}*/
                           value={data.cuitTitular}
                           disabled={disabledItems?.cuitTitular}
                           onChange={(value) =>
@@ -1668,38 +1670,38 @@ const GestionOSForm = ({
                       {(titular?.existeEnUATRE ||
                         titular?.existeEnOSPRERA ||
                         titular?.existeEnAFIP) && (
-                        <div>
-                          <h6
-                            style={{
-                              fontSize: "small",
-                              displa:
-                                titular?.existeEnUATRE ||
-                                (!!titular?.existeEnUATRE &&
-                                  titular.existeEnOSPRERA &&
-                                  !!titular.existeEnAFIP)
-                                  ? "none"
-                                  : "flex",
-                            }}
-                          >
-                            {" "}
-                            {titular.existeEnOSPRERA
-                              ? "Titular en Padron OSPRERA"
-                              : titular.existeEnAFIP
-                              ? "Titular en ARCA"
-                              : ""}{" "}
-                          </h6>
-                          <h6 style={{ fontSize: "small" }}>
-                            {titular.existeEnUATRE === true
-                              ? "Afiliado a UATRE"
-                              : titular.existeEnOSPRERA === null &&
-                                titular.existeEnAFIP === null
-                              ? "" //"No se encontraron datos para el CUIL ingresado"
-                              : titular.existeEnUATRE === false
-                              ? "No Afiliado a UATRE"
-                              : ""}
-                          </h6>
-                        </div>
-                      )}
+                          <div>
+                            <h6
+                              style={{
+                                fontSize: "small",
+                                displa:
+                                  titular?.existeEnUATRE ||
+                                    (!!titular?.existeEnUATRE &&
+                                      titular.existeEnOSPRERA &&
+                                      !!titular.existeEnAFIP)
+                                    ? "none"
+                                    : "flex",
+                              }}
+                            >
+                              {" "}
+                              {titular.existeEnOSPRERA
+                                ? "Titular en Padron OSPRERA"
+                                : titular.existeEnAFIP
+                                  ? "Titular en ARCA"
+                                  : ""}{" "}
+                            </h6>
+                            <h6 style={{ fontSize: "small" }}>
+                              {titular.existeEnUATRE === true
+                                ? "Afiliado a UATRE"
+                                : titular.existeEnOSPRERA === null &&
+                                  titular.existeEnAFIP === null
+                                  ? "" //"No se encontraron datos para el CUIL ingresado"
+                                  : titular.existeEnUATRE === false
+                                    ? "No Afiliado a UATRE"
+                                    : ""}
+                            </h6>
+                          </div>
+                        )}
                     </Grid>
                     <Grid col width="120px">
                       <Button
